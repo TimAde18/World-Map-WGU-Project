ef1ed0423652190ba2de6799e52990f1b0741d89
TimAde18 <aoadewole18@gmail.com>
2023-10-03 18:40:03 -0500

    added feat for highlighting country mouse is on

--- src/app/world-map/world-map.component.ts
+++ src/app/world-map/world-map.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-world-map',
   templateUrl: './world-map.component.html',
   styleUrls: ['./world-map.component.css']
 })
-export class WorldMapComponent {
+export class WorldMapComponent implements OnInit {
+  svgCountryPaths: SVGPathElement[] = [];
+
+  ngOnInit() {
+      this.svgCountryPaths = Array.from(document.querySelectorAll<SVGPathElement>('path'));
+      console.log(this.svgCountryPaths)
+
+      this.svgCountryPaths.forEach((pathElement) => {
+        pathElement.addEventListener('mouseenter',this.onHover);
+        pathElement.addEventListener('mouseleave', this.offHover);
+      })
+      
+  }
+
+  onHover(event: MouseEvent) {
+    const hoverColor = event.target as SVGElement;
+    hoverColor.style.fill='darkblue'
+  }
+
+  offHover(event: MouseEvent) {
+    const offHoverColor = event.target as SVGElement;
+    offHoverColor.style.fill='';
+  }
 
 }
