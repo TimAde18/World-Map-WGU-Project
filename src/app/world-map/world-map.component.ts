dad393947b922c9d5b8de0588e322573b3441ea7
TimAde18 <aoadewole18@gmail.com>
2023-10-04 11:47:26 -0500

    added properties for data. added click event

--- src/app/world-map/world-map.component.ts
+++ src/app/world-map/world-map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WorldBankApiService } from '../world-bank-api.service';
 
 @Component({
   selector: 'app-world-map',
@@ -7,26 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorldMapComponent implements OnInit {
   svgCountryPaths: SVGPathElement[] = [];
+  countryName: string = '';
+  capitalCity: string = '';
+  countryRegion: string = '';
+  incomeLevel: string = '';
+  longitude: string = '';
+  latitude: string= '';
+
+
+
+  constructor(private WorldBankApiService: WorldBankApiService) { }
 
   ngOnInit() {
       this.svgCountryPaths = Array.from(document.querySelectorAll<SVGPathElement>('path'));
-      console.log(this.svgCountryPaths)
 
       this.svgCountryPaths.forEach((pathElement) => {
         pathElement.addEventListener('mouseenter',this.onHover);
         pathElement.addEventListener('mouseleave', this.offHover);
-      })
+        pathElement.addEventListener('click', this.onClick.bind(this));
+      });
       
   }
 
   onHover(event: MouseEvent) {
     const hoverColor = event.target as SVGElement;
-    hoverColor.style.fill='darkblue'
+    hoverColor.style.fill='indigo'
   }
 
   offHover(event: MouseEvent) {
     const offHoverColor = event.target as SVGElement;
     offHoverColor.style.fill='';
+  }
+
+  onClick(event: MouseEvent) {
+    const country = event.target as SVGPathElement;
+    const idOfCountry = country.id;
+    this.WorldBankApiService.getCountryData(idOfCountry).subscribe((info) => {
+      this.countryName = info[1][0].name;
+      this.capitalCity = info[1][0].capitalCity;
+      this.countryRegion = info[1][0].region.value;
+      this.incomeLevel = info[1][0].incomeLevel.value;
+      this.longitude = info[1][0].longitude;
+      this.latitude = info[1][0].latitude;
+    });
   }
 
 }
