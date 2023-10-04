c567c04f7a892480351fe94980ccc98f20b01ac1
TimAde18 <aoadewole18@gmail.com>
2023-10-04 11:46:10 -0500

    created service class for API call

---
+++ src/app/world-bank-api.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WorldBankApiService {
+  private apiUrl = 'http://api.worldbank.org/v2/country';
+
+  constructor(private http: HttpClient) { }
+
+  getCountryData(countryCode: string): Observable<any> {
+    const url= `${this.apiUrl}/${countryCode}?format=json`;
+
+    return this.http.get(url);
+  }
+
+}
