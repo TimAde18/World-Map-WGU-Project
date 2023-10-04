6bf80e0c1e482b0b5710ca6236e1851fe212f963
TimAde18 <aoadewole18@gmail.com>
2023-10-04 11:45:12 -0500

    added import and provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WorldMapComponent } from './world-map/world-map.component';
+import { WorldBankApiService } from './world-bank-api.service';
+
 
 @NgModule({
   declarations: [
@@ -12,9 +15,10 @@ import { WorldMapComponent } from './world-map/world-map.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [WorldBankApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
